bb562d30dfce493b61fafe04b26d99b20f21227d
Hugo Puhlmann <hugopuhlmann@gmail.com>
2016-04-24 19:24:03 -0300

    Flushing IO buffers in the log macro.

--- src/shared/Debug.hpp
+++ src/shared/Debug.hpp
@@ -1,5 +1,11 @@
 #pragma once
 #include <cstdio>
-#define LOG(x) fprintf(stdout, x)
-#define LOG_W(x) fprintf(stdout, x)
-#define LOG_E(x) fprintf(stderr, x)
+#define LOG(x)                                                                 \
+  fprintf(stdout, "%s\n", x);                                                  \
+  fflush(stdout)
+#define LOG_W(x)                                                               \
+  fprintf(stdout, "%s\n", x);                                                  \
+  fflush(stdout)
+#define LOG_E(x)                                                               \
+  fprintf(stderr, "%s\n", x);                                                  \
+  fflush(stderr)

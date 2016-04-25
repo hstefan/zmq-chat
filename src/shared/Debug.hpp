e91d7576678d28aef8d0220cd068d175e9e898f5
Hugo Puhlmann <hugopuhlmann@gmail.com>
2016-04-24 23:42:01 -0300

    Making log macros safer to use.

--- src/shared/Debug.hpp
+++ src/shared/Debug.hpp
@@ -1,14 +1,20 @@
 #pragma once
 #include <cstdio>
 #define LOG_F(fmt, ...)                                                        \
-  fprintf(stdout, fmt, ##__VA_ARGS__);                                         \
-  fflush(stdout)
+  do {                                                                         \
+    fprintf(stdout, fmt, ##__VA_ARGS__);                                       \
+    fflush(stdout);                                                            \
+  } while (0)
 #define LOG_WF(fmt, ...)                                                       \
-  fprintf(stdout, fmt, ##__VA_ARGS__);                                         \
-  fflush(stdout)
+  do {                                                                         \
+    fprintf(stdout, fmt, ##__VA_ARGS__);                                       \
+    fflush(stdout);                                                            \
+  } while (0)
 #define LOG_EF(fmt, ...)                                                       \
-  fprintf(stdout, fmt, ##__VA_ARGS__);                                         \
-  fflush(stderr)
+  do {                                                                         \
+    fprintf(stderr, fmt, ##__VA_ARGS__);                                       \
+    fflush(stderr);                                                            \
+  } while (0)
 #define LOG(x) LOG_F("%s\n", x)
 #define LOG_W(x) LOG_WF("%s\n", x)
 #define LOG_E(x) LOG_EF("%s\n", x)

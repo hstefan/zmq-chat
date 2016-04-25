2310bde32a632ae1c983318d179d6b52ce9336ad
Hugo Puhlmann <hugopuhlmann@gmail.com>
2016-04-24 23:23:52 -0300

    Adding formated log macros.

--- src/shared/Debug.hpp
+++ src/shared/Debug.hpp
@@ -1,11 +1,14 @@
 #pragma once
 #include <cstdio>
-#define LOG(x)                                                                 \
-  fprintf(stdout, "%s\n", x);                                                  \
+#define LOG_F(fmt, ...)                                                        \
+  fprintf(stdout, fmt, ##__VA_ARGS__);                                         \
   fflush(stdout)
-#define LOG_W(x)                                                               \
-  fprintf(stdout, "%s\n", x);                                                  \
+#define LOG_WF(fmt, ...)                                                       \
+  fprintf(stdout, fmt, ##__VA_ARGS__);                                         \
   fflush(stdout)
-#define LOG_E(x)                                                               \
-  fprintf(stderr, "%s\n", x);                                                  \
+#define LOG_EF(fmt, ...)                                                       \
+  fprintf(stdout, fmt, ##__VA_ARGS__);                                         \
   fflush(stderr)
+#define LOG(x) LOG_F("%s\n", x)
+#define LOG_W(x) LOG_WF("%s\n", x)
+#define LOG_E(x) LOG_EF("%s\n", x)
